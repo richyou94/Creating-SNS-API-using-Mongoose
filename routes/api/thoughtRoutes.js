c1a57474c82be077636eb9e897de359d22598b15
Richard You <richardyou94@gmail.com>
2022-11-22 23:29:48 -0800

    add tRoutes without any controller code

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const {} = require('../../controllers/thoughtController');
+
+// api/thoughts
+
+//api/thoughts/:thoughtId
+
+//api/thoughts/:thoughtId/reactions
+
+//api/thoughts/:thoughtId/reactions/:reactionId
+
+module.exports = router;
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,9 @@
+const router = require("express").Router();
+
+const {} = require("../../controllers/userController.js");
+
+//api/users
+
+//api/users/userId
+
+module.exports = router;
