533736881af929a446ffc1f0c2eac7f657a4d866
Richard You <richardyou94@gmail.com>
2022-11-22 22:55:48 -0800

    adding User.js with basic data types

---
+++ models/User.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+        },
+        email: {
+            type: String,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User',
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
