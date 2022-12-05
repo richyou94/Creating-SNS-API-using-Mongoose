d476d73dfebf46ddf008f0fcaa0c7f766074645e
RichardBass You <richardbassyou@RichardBasssMBP.lan>
2022-12-04 22:38:17 -0800

    update to recent, small adjust

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
-const { connect, connection } = require("mongoose");
+const mongoose = require('mongoose');
 
-const dbName = "";
+const dbName = "snsData";
 const connectionString =
   process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/${dbName}`;
 
-connect(connectionString, {
+mongoose.connect(connectionString, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+  userUnifiedTopology: true
+})
 
-module.exports = connection;
+module.exports = mongoose.connection;
